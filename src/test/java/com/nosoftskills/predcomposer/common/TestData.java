32b4c82a0e2401b04451b6f17f99f954c0443445
ivannov <ivan.st.ivanov@gmail.com>
2017-01-25 09:30:50 +0200

    Update test data with 2016/2017 matches

--- src/test/java/com/nosoftskills/predcomposer/common/TestData.java
+++ src/test/java/com/nosoftskills/predcomposer/common/TestData.java
@@ -34,18 +34,18 @@ public final class TestData {
         initialize();
     }
 
-    public static void initialize() {
-        user1 = new User("ivan", hashPassword("ivan"), "ivan@example.com", "Ivan", "Ivanov", true);
-        user2 = new User("koko", hashPassword("koko"), "koko@example.com", "Koko", "Stefanov", false);
+    static void initialize() {
+        user1 = new User("ivan", hashPassword("ivan"), "ivan@nosoftskills.com", "Ivan", "Ivanov", true);
+        user2 = new User("koko", hashPassword("koko"), "koko@nosoftskills.com", "Koko", "Stefanov", false);
 
-        game1 = new Game("Manchester United", "Club Brugge", "3:1", LocalDateTime
-                .of(2015, 8, 18, 21, 45), true);
-        game2 = new Game("Club Brugge", "Manchester United", "0:4", LocalDateTime.of(2015, 8,
-                26, 21, 45), true);
-        game3 = new Game("Paris SG", "Malmo", LocalDateTime.of(2015, 9, 15, 21, 45));
-        game4 = new Game("Real Madrid", "Shakhtar Donetsk", LocalDateTime.of(2015, 9, 15, 21, 45));
-        game5 = new Game("Leverkusen", "BATE", LocalDateTime.of(2015, 9, 16, 21, 45));
-        game6 = new Game("Roma", "Barcelona", LocalDateTime.of(2015, 9, 16, 21, 45));
+        game1 = new Game("Porto", "Roma", "1:1", LocalDateTime
+                .of(2016, 8, 17, 21, 45), true);
+        game2 = new Game("Roma", "Porto", "0:3", LocalDateTime.of(2016, 8,
+                23, 21, 45), true);
+        game3 = new Game("Paris Saint-Germain", "Arsenal", LocalDateTime.of(2016, 9, 13, 21, 45));
+        game4 = new Game("Barcelona", "Celtic", LocalDateTime.of(2016, 9, 13, 21, 45));
+        game5 = new Game("Real Madrid", "Sporting", LocalDateTime.of(2016, 9, 14, 21, 45));
+        game6 = new Game("Juventus", "Sevilla", LocalDateTime.of(2015, 9, 14, 21, 45));
 
         prediction1 = new Prediction(user1, game2, "0:0");
         prediction2 = new Prediction(user2, game2, "0:2");
@@ -57,7 +57,7 @@ public final class TestData {
         game2.getPredictions().addAll(Arrays.asList(prediction1, prediction2));
         game3.getPredictions().add(prediction3);
 
-        competition = new Competition("Champions League 2015-2016");
+        competition = new Competition("Champions League 2016-2017");
         competition.getGames().addAll(Arrays.asList(game1, game2, game3, game4, game5, game6));
     }
 
